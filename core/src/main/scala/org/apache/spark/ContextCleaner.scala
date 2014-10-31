42664bbe266896a3abba75e7f1214768555d1f67
GuoQiang Li <witgo@qq.com>
2014-10-31 14:24:24 +0800

    Automatically cleanup checkpoint

--- core/src/main/scala/org/apache/spark/ContextCleaner.scala
+++ core/src/main/scala/org/apache/spark/ContextCleaner.scala
@@ -22,7 +22,7 @@ import java.lang.ref.{ReferenceQueue, WeakReference}
 import scala.collection.mutable.{ArrayBuffer, SynchronizedBuffer}
 
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.{RDDCheckpointData, RDD}
 import org.apache.spark.util.Utils
 
 /**
@@ -33,6 +33,7 @@ private case class CleanRDD(rddId: Int) extends CleanupTask
 private case class CleanShuffle(shuffleId: Int) extends CleanupTask
 private case class CleanBroadcast(broadcastId: Long) extends CleanupTask
 private case class CleanAccum(accId: Long) extends CleanupTask
+private case class CleanRDDCheckpointData(rddId: Int) extends CleanupTask
 
 /**
  * A WeakReference associated with a CleanupTask.
@@ -129,6 +130,11 @@ private[spark] class ContextCleaner(sc: SparkContext) extends Logging {
     registerForCleanup(broadcast, CleanBroadcast(broadcast.id))
   }
 
+  /** Register a RDDCheckpointData for cleanup when it is garbage collected. */
+  def registerRDDCheckpointDataForCleanup[T](rdd: RDD[_], parentId: Int) {
+    registerForCleanup(rdd, CleanRDDCheckpointData(parentId))
+  }
+
   /** Register an object for cleanup. */
   private def registerForCleanup(objectForCleanup: AnyRef, task: CleanupTask) {
     referenceBuffer += new CleanupTaskWeakReference(task, objectForCleanup, referenceQueue)
@@ -152,6 +158,8 @@ private[spark] class ContextCleaner(sc: SparkContext) extends Logging {
               doCleanupBroadcast(broadcastId, blocking = blockOnCleanupTasks)
             case CleanAccum(accId) =>
               doCleanupAccum(accId, blocking = blockOnCleanupTasks)
+            case CleanRDDCheckpointData(rddId) =>
+              doCleanRDDCheckpointData(rddId, blocking = blockOnCleanupTasks)
           }
         }
       } catch {
@@ -206,6 +214,17 @@ private[spark] class ContextCleaner(sc: SparkContext) extends Logging {
       logInfo("Cleaned accumulator " + accId)
     } catch {
       case e: Exception => logError("Error cleaning accumulator " + accId, e)
+    }
+  }  
+
+  def doCleanRDDCheckpointData(rddId: Int, blocking: Boolean) {
+    try {
+      logDebug("Cleaning rdd checkpoint data " + rddId)
+      RDDCheckpointData.clearRDDCheckpointData(sc,rddId, blocking)
+      logInfo("Cleaned rdd checkpoint data " + rddId)
+    }
+    catch {
+      case e: Exception => logError("Error cleaning rdd checkpoint data " + rddId, e)
     }
   }
 
